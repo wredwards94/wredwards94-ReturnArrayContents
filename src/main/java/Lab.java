45811980896990c6ffe28c7d1e602aaac5d16b21
Wesley Edwards <wredwards94@outlook.com>
2023-03-03 16:12:17 +0000

    lab complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,10 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String str = "";
+        for(int i = 0; i < nums.length; i++) {
+            str = str.concat(nums[i] + " ");
+        }
+        return str;
     }
 }
